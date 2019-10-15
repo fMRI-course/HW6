f67edf1386411f8465705d2807c6a00fff2fafab
Peiwu Qin <qaydm1979@gmail.com>
2019-10-14 23:12:27 -0700

    Hw6 solution

--- spm_funcs.py
+++ spm_funcs.py
@@ -20,6 +20,8 @@ or better, in IPython::
 """
 # Python 2 compatibility
 from __future__ import print_function, division
+import numpy as np
+import nibabel as nib
 
 # Any imports you need
 +++your code here+++
